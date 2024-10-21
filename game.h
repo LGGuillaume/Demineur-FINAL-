3a264482ffc821593f05b0513018839b3fdc08cc
codestar <glegat@gaming.tech>
2024-10-21 12:53:40 +0200

    Create game.h

---
+++ game.h
@@ -0,0 +1,24 @@
+#ifndef GAME_H
+#define GAME_H
+
+struct Game 
+{
+    int lines;
+    int columns;
+    char** board;
+    int numBombs;
+    char** revealed;
+
+    Game() : lines(0), columns(0), board(nullptr), numBombs(0), revealed(nullptr) {}
+    ~Game();
+
+    void initializeGame(int& lines, int& columns);
+    void displayBoard(int lines, int columns);
+    void userPosition(int& enterLine, char& enterColumn, int lines, int columns);
+    void placeBombs();
+    bool hasHitBomb(int enterLine, char enterColumn);
+    void adjacentBombs();
+
+};
+
+#endif
